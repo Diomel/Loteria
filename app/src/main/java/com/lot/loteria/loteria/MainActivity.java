3c042475c8b3527e1e4de757da9f37dc54173fc0
Henry Lorenzo <hlorenzo93@hotmail.com>
2016-06-21 12:23:16 -0400

    Upload file

---
+++ app/src/main/java/com/lot/loteria/loteria/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lot.loteria.loteria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
