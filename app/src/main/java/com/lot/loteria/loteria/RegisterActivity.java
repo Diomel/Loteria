589f5da77c6ae918b75be069d0bba86e3ebd8dd2
Henry Lorenzo <hlorenzo93@hotmail.com>
2016-07-05 13:26:17 -0400

    Modify everything. Change for a better project structure

--- app/src/main/java/com/lot/loteria/loteria/RegisterActivity.java
+++ app/src/main/java/com/lot/loteria/loteria/RegisterActivity.java
@@ -3,7 +3,7 @@ package com.lot.loteria.loteria;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Register extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/lot/loteria/loteria/UserAreaActivity.java
+++ app/src/main/java/com/lot/loteria/loteria/UserAreaActivity.java
@@ -1,13 +1,13 @@
 package com.lot.loteria.loteria;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class Login extends AppCompatActivity {
+public class UserAreaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
