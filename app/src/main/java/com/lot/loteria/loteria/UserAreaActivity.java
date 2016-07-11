90e6db02bc06d9b09365481b434cd2fe14611b53
Henry Lorenzo <hlorenzo93@hotmail.com>
2016-07-11 12:07:41 -0400

    Updates 11/6/2016

--- app/src/main/java/com/lot/loteria/loteria/UserAreaActivity.java
+++ app/src/main/java/com/lot/loteria/loteria/UserAreaActivity.java
@@ -1,7 +1,10 @@
 package com.lot.loteria.loteria;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserAreaActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class UserAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText etAge = (EditText) findViewById(R.id.etAge);
+        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+
+        final TextView welcomMessage = (TextView) findViewById(R.id.tvWelcommsg);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        int age = intent.getIntExtra("age", - 1);
+
+        String message = name + " welcome to your user area";
+        welcomMessage.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age + "");
     }
 }
